142ee11a74e1550cf63f2ce7fcb868dd18154d31
Mohit <your_github_email@gmail.com>
2026-03-02 20:40:54 +0530

    Fix NLTK corpus download for deployment

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import pickle
 from src.preprocess import clean_text
 import re
 import time
+import nltk
+
+nltk.download("stopwords")
+nltk.download("wordnet")
+nltk.download("omw-1.4")
+nltk.download("words")
 
 model = pickle.load(open("model/model.pkl", "rb"))
 tfidf = pickle.load(open("model/tfidf.pkl", "rb"))
